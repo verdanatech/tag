db4b35f89c4ecb625cb86643e809c6ddec67fd83
Márcio Fernandes <marcio.fernandes@verdanatech.com>
2023-04-13 15:21:17 -0300

    :spakles: Add new version

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '2.11.1',
+        'version' => '2.11.1.0',
+        'reference' => 'e2d9c9298be7d2df07330351eae69110ebde5572',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '2.11.1',
+            'version' => '2.11.1.0',
+            'reference' => 'e2d9c9298be7d2df07330351eae69110ebde5572',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
